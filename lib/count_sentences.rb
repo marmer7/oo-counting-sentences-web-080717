407c5f996a01c7dcc6d5b36bf32d8145e8f4441d
Marlon Merjos <marlonmerjos@gmail.com>
2017-08-10 09:05:53 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[-1] == "."
+      return true
+    end
+    return false
   end
 
   def question?
-
+    if self[-1] == "?"
+      return true
+    end
+    return false
   end
 
   def exclamation?
-
+    if self[-1] == "!"
+      return true
+    end
+    return false
   end
 
   def count_sentences
-
+    self.split(/[!.?]/).delete_if {|w| w.size < 2}.size
   end
 end
